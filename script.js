ec75d85ac42c8cceccc7c574b6561e4a32de5553
angeloqq03 <124446133+angeloqq03@users.noreply.github.com>
2024-02-29 19:51:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,12 @@
-var audio = new Audio('sentmessage.mp3');
-var audioContext;
-var mediaRecorder;
-var audioChunks = [];
-
-const maintenance = "Maintenance ongoing, available by 02-29-2024, at 01:00AM PST";
-
-/*
-var contactAngelo = "<div class='social'> <a target='_blank' href='tel:+639670971677'> <div class='socialItem' id='call'><img class='socialItemI' src='phone.svg'/><label class='number'></label></label></div> </a> <a href='mailto:angeloqq03@gmail.com'> <div class='socialItem'><img class='socialItemI' src='gmail.svg' alt=''></div> </a> <a target='_blank' href='https://github.com/angeloqq03'> <div class='socialItem'><img class='socialItemI' src='github.svg' alt=''></div> </a> <a target='_blank' href='https://wa.me/639670971677'> <div class='socialItem'><img class='socialItemI' src='whatsapp.svg' alt=''>";
-
-var addressString = "";
-
-const DATA = {
-  goverment:{
-    psa:{
-      a:["Click the link to proceed on <a href='psa.com'>Philippines Statistics Authority </a>"]
-    }
-  },
-  
-  social:{
-    facebook:[],
-  },
-  
-  form:{
-    form1:[''],
-  },
-  
-  selector:{
-    selector1:['selector here with options']
-  }
-}
-
-*/
-
-const conversation = {
-  stage: 0,
-  applicantInfo: {
-    firstName: "",
-    lastName: "",
-    middleName: "",
-    age: "",
-    email: "",
-    phoneNumber: "",
-    address: "",
-    code: ""
-  }
-};
-
-
-
 function startFunction() {
     setLastSeen();
     wNreply('intro');
 }
-
-
-
 function setLastSeen() {
     var date = new Date();
     var lastSeen = document.getElementById("lastseen");
     lastSeen.innerText = "last seen today at " + date.getHours() + ":" + date.getMinutes()
 }
-
-
 function closeFullDP() {
     var x = document.getElementById("fullScreenDP");
     if (x.style.display === 'flex') {
@@ -70,7 +15,6 @@ function closeFullDP() {
         x.style.display = 'flex';
     }
 }
-
 function openFullScreenDP() {
     var x = document.getElementById("fullScreenDP");
     if (x.style.display === 'flex') {
@@ -79,13 +23,11 @@ function openFullScreenDP() {
         x.style.display = 'flex';
     }
 }
-
 function isEnter(event) {
     if (event.keyCode == 13) {
         sendMsg();
     }
 }
-
 function sendMsg() {
     var input = document.getElementById("inputMSG");
     var ti = input.value;
@@ -112,9 +54,6 @@ function sendMsg() {
     input.value = "";
     playSound();
 }
-
-
-
 function wNreply(inputText) {
     var lastSeen = document.getElementById("lastseen");
     lastSeen.innerText = "typing...";
@@ -123,41 +62,48 @@ function wNreply(inputText) {
     let user = inputText.toLowerCase();
 
     if (user.includes("my name is")) {
-        name = user.substring(user.indexOf("is") + 2);
+        name = user.substring(user.indexOf("is") + 1);
         if (name.toLowerCase().includes("angelo")) {
-            sendTextMessage("Ohh! That's my name too");
+            botM("Ohh! That's my name too");
         }
         user = "input";
-    } else if (user.includes("adonis")) {
-      
-       // sendTextMessage("Eto sample <a href='adonis.zip' target='_blank'> I CLICK MOKO UY </a>");
-       // conversation.stage = 8;
+    } else if (user.includes("what's")||user.includes('what')) {
+        if (user.includes("is")) {
+          if(user.includes('name')) {
+            botM('1')
+          } else if (user.includes('hobby')) {
+            botM('2')
+          }
+        } else if (user.includes('are')) {
+          
+        }
         return;
-    } else if (user.includes('fb')) {
-       // sendTextMessage(DATA.form.form1);
+    } else if (user.includes('adonis')) {
+       // botM(DATA.form.form1);
+       botM('<a href="adonis.zip" target="_blank">CLICK ME KASE</a>')
         return maintenance;
     } else if (user.startsWith('a-')) {
-        checkApplicationStatus(user.substr(2)); // Extract the code after "A-"
+        checkApstts(user.substr(2)); // Extract the code after "A-"
         return;
     }
 
     switch (user.trim()) {
-      /*
+    
         case "help":
-           sendTextMessage("<span class='sk'>You may send the following basic keywords<br>e.g<br><span class='bold'>'Apply'</span> - for applicants. <br><span class='bold'>'Interested'</span> - to know more about the process of application. <br><span class='bold'>'Newbie'</span> - if you are new in the world of Customer Service. <br><span class='bold'>'Contact'</span> - reach out to me personally. <br><span class='bold'>'FAQ's</span> - to see the most frequently asked questions about BPO Industry<br><span class='bold'>'Clear'</span> - to clear conversation<br><br> - Or you can just ask me anything that is Customer Service Related.<br>");
+           botM("<span class='sk'>You may send the following basic keywords<br>e.g<br><span class='bold'>'Apply'</span> - for applicants. <br><span class='bold'>'Interested'</span> - to know more about the process of application. <br><span class='bold'>'Newbie'</span> - if you are new in the world of Customer Service. <br><span class='bold'>'Contact'</span> - reach out to me personally. <br><span class='bold'>'FAQ's</span> - to see the most frequently asked questions about BPO Industry<br><span class='bold'>'Clear'</span> - to clear conversation<br><br> - Or you can just ask me anything that is Customer Service Related.<br>");
             break;
             
         case "resume":
-          //  sendTextMessage(resumeString);
+          //  botM(resumeString);
             break;
-            */
+            
         case "intro":
-              sendTextMessage(maintenance);
+              botM(r.intro);
             break;
-        /*
+        
         case "i want to apply":
         case "applicant":
-            sendTextMessage("To get started, please type your first name.");
+            botM("To get started, please type your first name.");
             conversation.stage = 1;
             break;
         case "clear":
@@ -165,94 +111,88 @@ function wNreply(inputText) {
             break;
         case "time":
             var date = new Date();
-            sendTextMessage("Current time is " + date.getHours() + ":" + date.getMinutes());
+            botM("Current time is " + date.getHours() + ":" + date.getMinutes());
             break;
         case "date":
             var date = new Date();
-            sendTextMessage("Current date is " + date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear());
+            botM("Current date is " + date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear());
             break;
         default:
             if (conversation.stage > 0) {
                 // Handle collecting applican
-                processApplicantInfo(user.trim());
+                processaplcntInf(user.trim());
             } else {
-                sendTextMessage('Im quite not sure I understand that. You can try other term for that or just type "help" to see the other available commands.');
+                botM('Im quite not sure I understand that. You can try other term for that or just type "help" to see the other available commands.');
             }
             break;
-            */
+            
     }
 }
-
-/*
-function processApplicantInfo(input) {
+function processaplcntInf(input) {
     switch (conversation.stage) {
       
         case 1:
-            conversation.applicantInfo.firstName = input;
+            conversation.aplcntInf.firstName = input;
             conversation.stage = 2;
-            sendTextMessage("Next, please type your last name.");
+            botM("Next, please type your last name.");
             break;
         case 2:
-            conversation.applicantInfo.lastName = input;
+            conversation.aplcntInf.lastName = input;
             conversation.stage = 3;
-            sendTextMessage("Please provide your middle name (if any), or type 'none'.");
+            botM("Please provide your middle name (if any), or type 'none'.");
             break;
         case 3:
             if (input.toLowerCase() === "none") {
-                conversation.applicantInfo.middleName = "None";
+                conversation.aplcntInf.middleName = "None";
             } else {
-                conversation.applicantInfo.middleName = input;
+                conversation.aplcntInf.middleName = input;
             }
             conversation.stage = 4;
-            sendTextMessage("What is your age?");
+            botM("What is your age?");
             break;
         case 4:
-            conversation.applicantInfo.age = input;
+            conversation.aplcntInf.age = input;
             conversation.stage = 5;
-            sendTextMessage("Please enter your email address.");
+            botM("Please enter your email address.");
             break;
         case 5:
-            conversation.applicantInfo.email = input;
+            conversation.aplcntInf.email = input;
             conversation.stage = 6;
-            sendTextMessage("Next, please provide your phone number.");
+            botM("Next, please provide your phone number.");
             break;
         case 6:
-            conversation.applicantInfo.phoneNumber = input;
+            conversation.aplcntInf.phoneNumber = input;
             conversation.stage = 7;
-            sendTextMessage("Finally, type your present address.");
+            botM("Finally, type your present address.");
             break;
         case 7:
-            conversation.applicantInfo.address = input;
-            conversation.applicantInfo.code = generateUniqueCode();
-            saveApplicantInfo(conversation.applicantInfo); // Call a function
+            conversation.aplcntInf.address = input;
+            conversation.aplcntInf.code = generateUniqueCode();
+            svapplntInfo(conversation.aplcntInf); // Call a function
             conversation.stage = 0; // Reset
-            sendTextMessage("Thank you! Your application has been submitted. To track the status of your application, here is a unique code: A-" + conversation.applicantInfo.code + ". Just send 'A-" + conversation.applicantInfo.code + "' to check.");
+            botM("Thank you! Your application has been submitted. To track the status of your application, here is a unique code: A-" + conversation.aplcntInf.code + ". Just send 'A-" + conversation.aplcntInf.code + "' to check.");
             break;
         case 8:
             processAssessmentCode(input);
             break;
         default:
-            sendTextMessage("I'm sorry, I didn't understand that.");
+            botM("I'm sorry, I didn't understand that.");
             break;
     }
+    console.log(user);
 }
-*/
-
-
-/*
 function processAssessmentCode(input) {
     if (conversation.stage === 8) {
-        conversation.applicantInfo.code = input;
+        conversation.aplcntInf.code = input;
         conversation.stage = 9;
-        sendTextMessage("Great! Now get ready to listen to the audio. Please click the link to play the audio: <a href='path_to_your_audio_file'>Play Audio</a>");
+        botM("Great! Now get ready to listen to the audio. Please click the link to play the audio: <a href='path_to_your_audio_file'>Play Audio</a>");
     } else if (conversation.stage === 9) {
-        // Placeholder for voice recording
-        setTimeout(startVoiceRecording, 2000); // Start recording after 2 seconds delay
+        // Placeholder
+        setTimeout(startVoiceRecording, 2000); // Start recording
     } else {
-        sendTextMessage("I'm sorry, I didn't understand that.");
+        botM("I'm sorry, I didn't understand that.");
     }
 }
-
 function startVoiceRecording() {
     navigator.mediaDevices.getUserMedia({ audio: true })
         .then(function(stream) {
@@ -267,42 +207,29 @@ function startVoiceRecording() {
             mediaRecorder.onstop = function() {
                 const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
                 const audioUrl = URL.createObjectURL(audioBlob);
-                saveAudioRecording(conversation.applicantInfo.code, audioUrl);
+                saveAudioRecording(conversation.aplcntInf.code, audioUrl);
                 audioChunks = [];
             };
 
             mediaRecorder.start();
-            sendTextMessage("Recording started. Click 'Stop' when you are finished.");
+            botM("Recording started. Click 'Stop' when you are finished.");
         })
         .catch(function(err) {
             console.error('Error accessing microphone:', err);
         });
 }
-
 function stopRecording() {
     if (mediaRecorder && mediaRecorder.state === "recording") {
         mediaRecorder.stop();
-        sendTextMessage("Recording stopped.");
+        botM("Recording stopped.");
     }
 }
-
-
-
 function saveAudioRecording(code, audioUrl) {
-    // Here you would handle saving the audio recording to your database
-    // You can use XMLHttpRequest or fetch API to send the audio data to your server
-    // For this example, we're just logging the code and audio URL
     console.log("Code:", code);
     console.log("Audio URL:", audioUrl);
-    sendTextMessage("Audio recording saved successfully.");
-
-    //send the audio URL 
+    botM("Audio recording saved successfully.");
     sendAudioMessage(audioUrl);
 }
-*/
-
-
-/*
 function sendAudioMessage(audioUrl) {
     const audioElement = document.createElement("audio");
     audioElement.controls = true;
@@ -316,14 +243,11 @@ function sendAudioMessage(audioUrl) {
     var s = document.getElementById("chatting");
     s.scrollTop = s.scrollHeight;
 }
-
 function clearChat() {
     document.getElementById("listUL").innerHTML = "";
     wNreply('intro');
 }
-*/
-
-function sendTextMessage(textToSend) {
+function botM(textToSend) {
     setTimeout(setLastSeen, 1000);
     var date = new Date();
     var myLI = document.createElement("li");
@@ -344,13 +268,9 @@ function sendTextMessage(textToSend) {
     s.scrollTop = s.scrollHeight;
     playSound();
 }
-
 function playSound() {
     audio.play();
 }
-
-/*
-
 function generateUniqueCode() {
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var code = '';
@@ -359,8 +279,7 @@ function generateUniqueCode() {
     }
     return code;
 }
-
-function saveApplicantInfo(applicantInfo) {
+function svapplntInfo(aplcntInf) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "save_applicant_info.php", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -369,19 +288,18 @@ function saveApplicantInfo(applicantInfo) {
             console.log(xhr.responseText); // Log server response
         }
     };
-    var data = "firstName=" + encodeURIComponent(applicantInfo.firstName) +
-               "&lastName=" + encodeURIComponent(applicantInfo.lastName) +
-               "&middleName=" + encodeURIComponent(applicantInfo.middleName) +
-               "&age=" + encodeURIComponent(applicantInfo.age) +
-               "&email=" + encodeURIComponent(applicantInfo.email) +
-               "&phoneNumber=" + encodeURIComponent(applicantInfo.phoneNumber) +
-               "&address=" + encodeURIComponent(applicantInfo.address) +
-               "&code=" + encodeURIComponent(applicantInfo.code);
+    var data = "firstName=" + encodeURIComponent(aplcntInf.firstName) +
+               "&lastName=" + encodeURIComponent(aplcntInf.lastName) +
+               "&middleName=" + encodeURIComponent(aplcntInf.middleName) +
+               "&age=" + encodeURIComponent(aplcntInf.age) +
+               "&email=" + encodeURIComponent(aplcntInf.email) +
+               "&phoneNumber=" + encodeURIComponent(aplcntInf.phoneNumber) +
+               "&address=" + encodeURIComponent(aplcntInf.address) +
+               "&code=" + encodeURIComponent(aplcntInf.code);
     xhr.send(data);
 }
-
-function checkApplicationStatus(code) {
-    sendTextMessage("Checking application status for code: " + code);
+function checkApstts(code) {
+    botM("Checking application status for code: " + code);
 
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "get_interview_schedule.php", true);
@@ -392,17 +310,15 @@ function checkApplicationStatus(code) {
             if (response.success) {
                 var scheduleDate = response.scheduleDate;
                 var link = response.link;
-                sendTextMessage("Your application is currently scheduled for an Initial Interview on " + scheduleDate + ". Click here for more details: " + link);
+                botM("Your application is currently scheduled for an Initial Interview on " + scheduleDate + ". Click here for more details: " + link);
             } else {
-                sendTextMessage("No schedule found for the provided code.");
+                botM("No schedule found for the provided code.");
             }
         }
     };
     xhr.send("code=" + encodeURIComponent(code));
 }
-
-
-function getInterviewSchedule(code, callback) {
+function getIntSchdl(code, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "get_schedule.php", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -417,4 +333,4 @@ function getInterviewSchedule(code, callback) {
         }
     };
     xhr.send("code=" + encodeURIComponent(code));
-} */
+} 
